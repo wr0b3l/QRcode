8fdad255985ecf4f6342f4571b0b846997b1c057
Wr0b3l <l.wroblewski88@gmail.com>
2021-04-27 11:46:11 +0200

    Initial commit

---
+++ QRservice/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+import qrcode
+import qrcode.image.svg
+from io import BytesIO
+
+
+def index(request):
+    context = {}
+    if request.method == "POST":
+        factory = qrcode.image.svg.SvgImage
+        img = qrcode.make(
+            request.POST.get("qr_text", ""),
+            image_factory=factory,
+            box_size=20)
+        stream = BytesIO()
+        img.save(stream)
+        context["svg"] = stream.getvalue().decode()
+
+    return render(request, "index.html", context=context)
+
+# Create your views here.
